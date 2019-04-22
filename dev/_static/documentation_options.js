e81944407da24523dd8346235d6c9e31545cdbda
TravisCI <travis@nothing.com>
2019-04-22 04:59:03 +0000

    Link 'latest' to 'dev' until we make a release

---
+++ dev/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '6.0.0_7c87ef9_2019.04.18',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
